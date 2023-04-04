172b84619917267fa3ddcfffba2a10ac0e2ea6f5
sebas1310 <a20203368@pucp.edu.pe>
2023-04-04 14:01:08 -0500

    L2 proyecto

---
+++ src/main/java/com/example/l2_20221/L220221Application.java
@@ -0,0 +1,13 @@
+package com.example.l2_20221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L220221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L220221Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/l2_20221/L220221ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l2_20221;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L220221ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
